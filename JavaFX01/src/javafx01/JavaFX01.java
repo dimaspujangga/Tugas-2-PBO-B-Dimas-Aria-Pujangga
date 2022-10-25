5e4eba1a61b81899bd5fa94c2763a05f86cd2323
dimaspujangga <114992718+dimaspujangga@users.noreply.github.com>
2022-10-25 23:33:59 +0700

    Add files via upload

---
+++ JavaFX01/src/javafx01/JavaFX01.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafx01;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author dimas
+ */
+public class JavaFX01 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        //remove code from here 
+	Button btn = new Button();
+        btn.setText("Say 'HelloWorld'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	//to here to add your own code
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
